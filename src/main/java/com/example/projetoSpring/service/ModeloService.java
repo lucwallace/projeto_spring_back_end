319c91a75ff4dc50e82ff37d9b81f05f9e7d1cb3
Lucas Wallace <lucaswallace2016d@gmail.com>
2022-11-20 23:35:43 -0300

    Adição do update e delete no modelo.

--- src/main/java/com/example/projetoSpring/service/ModeloService.java
+++ src/main/java/com/example/projetoSpring/service/ModeloService.java
@@ -3,9 +3,13 @@ package com.example.projetoSpring.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import com.example.projetoSpring.domain.Marca;
 import com.example.projetoSpring.domain.Modelo;
+import com.example.projetoSpring.dto.ModeloDto;
+import com.example.projetoSpring.enums.TipoModeloEnum;
 import com.example.projetoSpring.repositories.ModeloRepository;
 import com.example.projetoSpring.service.exceptions.ObjectNotFoundException;
 
@@ -24,5 +28,29 @@ public class ModeloService {
 		obj.setId(null);
 		return re.save(obj);
 	}
+	
+	public Modelo fromDTO(ModeloDto objDto) {
+		Modelo mo = new Modelo(null, objDto.getNome(), objDto.getPreco(), TipoModeloEnum.toEnum(objDto.getIdTipo()));
+		Marca ma = new Marca(objDto.getMarcaId(), null);
+		
+		mo.getMarcas().add(ma);
+		
+		return mo;
+	}
+	
+	public Modelo update(Modelo obj) {
+		find(obj.getId());
+		return re.save(obj);
+	}
+	
+	public void delete(Modelo obj) {
+		find(obj.getId());
+		try {
+			re.delete(obj);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Não é possivel excluir modelo.");
+		}
+	}
+	
 
 }
